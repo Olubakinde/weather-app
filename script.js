427a58dc1f31ae7d0fcb522e3f151af7d646e99d
tolubaki <tolubaki@udel.edu>
2024-07-20 01:19:46 -0400

    Updated Uder Interface

--- script.js
+++ script.js
@@ -2,44 +2,57 @@ const apiKey = "e254d30c290f5fb05353e95f3cb0fc35";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".weather-icon")
+const weatherIcon = document.querySelector(".weather-icon");
+
+const backgrounds = {
+    Clear: 'url("./img/clear-bg.jpg")',
+    Clouds: 'url("./img/clouds-bg.jpg")',
+    Rain: 'url("./img/rain-bg.jpg")',
+    Drizzle: 'url("./img/drizzle-bg.jpg")',
+    Mist: 'url("./img/mist-bg.jpg")'
+};
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-    if (response.status == 404){
-        document.querySelector(".error").style.display  = "block"
-        document.querySelector(".weather").style.display  = "none"
+    if (response.status === 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
     } else {
-        var data = await response.json();
+        const data = await response.json();
+        const weatherCondition = data.weather[0].main;
 
-        document.querySelector(".city").innerHTML = data.name
+        document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
-    
-        if(data.weather[0].main == "Clouds"){
-            weatherIcon.src = "img/clouds.png"
-        } else if(data.weather[0].main == "Clear"){
-            weatherIcon.src = "img/clear.png"
-        } else if(data.weather[0].main == "Rain"){
-            weatherIcon.src = "img/rain.png"
-        } else if(data.weather[0].main == "Drizzle"){
-            weatherIcon.src = "img/drizzle.png"
-        }
-        else if(data.weather[0].main == "Mist"){
-            weatherIcon.src = "img/mist.png"
-        }
-    
-        document.querySelector(".weather").style.display = "block"
-        document.querySelector(".error").style.display = "none"
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
+        if (weatherCondition === "Clouds") {
+            weatherIcon.src = "img/clouds.png";
+        } else if (weatherCondition === "Clear") {
+            weatherIcon.src = "img/clear.png";
+        } else if (weatherCondition === "Rain") {
+            weatherIcon.src = "img/rain.png";
+        } else if (weatherCondition === "Drizzle") {
+            weatherIcon.src = "img/drizzle.png";
+        } else if (weatherCondition === "Mist") {
+            weatherIcon.src = "img/mist.png";
+        }
+
+        updateBackground(weatherCondition);
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
     }
 }
 
-searchBtn.addEventListener("click", ()=>{
+function updateBackground(weatherCondition) {
+    document.body.style.backgroundImage = backgrounds[weatherCondition] || '';
+}
+
+searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
-})
+});
 
-checkWeather();
-
+// Check weather for a default city on page load
+checkWeather("New York");
